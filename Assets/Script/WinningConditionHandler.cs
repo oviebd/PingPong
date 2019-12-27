05a4e7342922c4bbf2ba2b0e223339692bc69234
oviebd <oviecse48@gmail.com>
2019-12-27 16:39:32 +0600

    Add WinningCondition Handler
    Logic not implemented properly

---
+++ Assets/Script/WinningConditionHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinningConditionHandler : MonoBehaviour {
+
+	public WinningConditionHandler instance;
+	private int _winningPoint = 3;
+
+
+	void Start()
+	{
+		if (instance == null)
+			instance = new WinningConditionHandler();
+	}
+
+	public void SetWinningPoint(int winningPoint)
+	{
+		this._winningPoint = winningPoint;
+	}
+
+	void CheckWinningPoint()
+	{
+		if (ScoreManager.instance.GetRightSidePlayerScore() >= _winningPoint)
+		{
+			PlayerWin(GameEnums.PlayerEnum.Player1_Right);
+		}
+		if (ScoreManager.instance.GetLeftSidePlayerScore() >= _winningPoint)
+		{
+			PlayerWin(GameEnums.PlayerEnum.Player2_Left);
+		}
+	}
+
+	void PlayerWin(GameEnums.PlayerEnum winnerPlayer)
+	{
+
+	}
+}
